28d4046f28ba65950568b5c2a63dd38499a9a1d7
Guga's PC <Guga's PC@DESKTOP-2DB9EPL>
2025-06-07 08:43:35 -0700

    add push audio

---
+++ GLobal.Natia.Ge/Controllers/AudioStreamerController.cs
@@ -0,0 +1,31 @@
+﻿using Common.Domain.SignalR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace GLobal.Natia.Ge.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AudioStreamerController : ControllerBase
+{
+
+    private readonly IHubContext<UniteMonitoringHub> _hub;
+
+    public AudioStreamerController(IHubContext<UniteMonitoringHub> hub)
+    {
+        _hub = hub;
+    }
+
+    [HttpPost("push")]
+    public async Task<IActionResult> PushAudio([FromBody] byte[] wavBytes)
+    {
+        if (wavBytes == null || wavBytes.Length < 100)
+            return BadRequest("Invalid audio stream");
+
+        var base64Audio = Convert.ToBase64String(wavBytes);
+
+        await _hub.Clients.All.SendAsync("robotAudioStream", base64Audio);
+
+        return Ok("Audio pushed to clients.");
+    }
+}
