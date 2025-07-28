8af51b67c751eaa6d17c8ac4e9e90bca97cda6ee
Guga's PC <Guga's PC@DESKTOP-2DB9EPL>
2025-07-28 10:10:18 -0700

    Update temperature api

--- Common.Domain/Services/TemperatureService.cs
+++ Common.Domain/Services/TemperatureService.cs
@@ -6,21 +6,15 @@ namespace Common.Domain.Services;
 
 public class TemperatureService : ITemperatureService
 {
-    private const string serverUrl = "https://192.168.0.79:2000";
+    private const string serverUrl = "http://192.168.0.79:2000";
     public async Task<TemperatureModel> GetCUrrentTemperature()
     {
         try
         {
-            var handler = new HttpClientHandler
-            {
-                ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) => true
-            };
-            var client = new HttpClient(handler);
-
+            var client = new HttpClient();
             var response = await client.GetAsync($"{serverUrl}/api/Temprature/GetCurrentTemperature");
             response.EnsureSuccessStatusCode();
             var responseData = await response.Content.ReadAsStringAsync();
-
             var res=JsonSerializer.Deserialize<TemperatureModel>(responseData);
             if (res == null)
             {
